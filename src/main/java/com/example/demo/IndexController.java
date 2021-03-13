ae6f05215af8dc9e5224914b423b086acfa511d4
oeardi@126.com <classical.444>
2021-03-13 13:43:17 +0800

    init git commit

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 冷枫红舞
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping(value = "/index")
+    public String index() {
+        return "hello project";
+    }
+}
